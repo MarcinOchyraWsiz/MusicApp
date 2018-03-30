87e174be0aa553b0c33e94bc221d60d58ca5436f
Marcin Ochyra <mochyra@wsiz.rzeszow.pl>
2018-03-30 16:28:43 +0200

    Aktywnosc top utworow

--- app/src/main/java/pl/blogprogramisty/musicapp/MainActivity.java
+++ app/src/main/java/pl/blogprogramisty/musicapp/MainActivity.java
@@ -1,10 +1,12 @@
 package pl.blogprogramisty.musicapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+
+import pl.blogprogramisty.musicapp.topsongs.TopSongsActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,7 +19,8 @@ public class MainActivity extends AppCompatActivity {
         bTopSongs.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "TODO", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this, TopSongsActivity.class);
+                startActivity(intent);
             }
         });
     }
---
+++ app/src/main/java/pl/blogprogramisty/musicapp/topsongs/TopSongsActivity.java
@@ -0,0 +1,25 @@
+package pl.blogprogramisty.musicapp.topsongs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import pl.blogprogramisty.musicapp.R;
+
+public class TopSongsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top_songs);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle(R.string.top_songs);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+
+        return true;
+    }
+}
