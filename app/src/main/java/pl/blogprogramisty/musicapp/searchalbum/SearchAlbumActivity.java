f92be2f71bbd19d1ee64bb09eb4ff116a3e2b3c7
Marcin Ochyra <mochyra@wsiz.rzeszow.pl>
2018-05-21 18:51:18 +0200

    Szablon wyszukiwarki

--- app/src/main/java/pl/blogprogramisty/musicapp/searchalbum/SearchAlbumActivity.java
+++ app/src/main/java/pl/blogprogramisty/musicapp/searchalbum/SearchAlbumActivity.java
@@ -2,10 +2,18 @@ package pl.blogprogramisty.musicapp.searchalbum;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import pl.blogprogramisty.musicapp.R;
 
 public class SearchAlbumActivity extends AppCompatActivity {
+
+    EditText etQuery;
+    RecyclerView rvList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +21,17 @@ public class SearchAlbumActivity extends AppCompatActivity {
         setContentView(R.layout.activity_search_album);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        etQuery = findViewById(R.id.etQuery);
+        rvList = findViewById(R.id.rvList);
+
+        Button bSearch = findViewById(R.id.bSearch);
+        bSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(SearchAlbumActivity.this, "TODO", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
